ad53844c9edc9a1175e9d68408b8b4038f7faf51
Mayank Yadav <mayankyadav14@gmail.com>
2020-06-09 16:58:46 +0530

    Revert "Updates"

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import mockdata from './mock_data.json';
-import Card from './Components/Card/Card';
-
 
 function App() {
-  
   return (
     <div className="App">
-      <div className="cards-container">
-        {mockdata.map(item => <Card key={item.dateLastEdited} {...item} />)}
-      </div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
