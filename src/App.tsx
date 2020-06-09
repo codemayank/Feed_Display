b52dae0dfd8f0d50e2bee596a2c7fcb03c763615
Mayank Yadav <mayank.yadav@pariksha.co>
2020-06-09 18:59:08 +0530

    update 1. Add pagination 2. Add tests for pagination 3. add global types file

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+
+
+import React, {FC, useState} from 'react';
+import {PAGE_DATA_LIMIT as limit} from './constants';
+
 import './App.css';
-import mockdata from './mock_data.json';
+
 import Card from './Components/Card/Card';
 
 
-function App() {
+const App: FC<appProps> = ({mockData}) => {
   
+  let [offset, setOffset] = useState<number>(0)
+
+  
+  let displyedItems: cardData[] = mockData.slice(offset, offset + limit);
+  let totalPages: number = Math.ceil(mockData.length / limit);
+
+  let pages: page[] = [];
+  for(let i = 0; i < totalPages; i++){
+    let page = {
+      pageNo: i+1,
+      selected: offset === i
+    }
+    pages.push(page);
+  }
+
   return (
     <div className="App">
+      <div className="pages-navigation">
+        {pages.map(({pageNo, selected}) => <button data-testid="page-nav-btn" key={pageNo} onClick={() => setOffset(pageNo - 1)} className={selected ? "page-button selected" : "page-button"}>{pageNo}</button>)}
+      </div>
       <div className="cards-container">
-        {mockdata.map(item => <Card key={item.dateLastEdited} {...item} />)}
+        {displyedItems.map(item => <Card key={item.dateLastEdited} {...item} />)}
       </div>
     </div>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import mockData from './mock_data.json';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App mockData={mockData} />
   </React.StrictMode>,
   document.getElementById('root')
 );
